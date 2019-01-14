15ee408cf7de794793c1bae8c8e1e7848c752c19
Silvia García Cacho <silvia@garca.info>
2019-01-14 09:49:06 +0100

    Add docs folder to publicate github pages

---
+++ docs/precache-manifest.71b0cf8a38b709f271dc423daccc4baa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1ea739c8110b3f3079dd",
+    "url": "/static/js/main.1ea739c8.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "1ea739c8110b3f3079dd",
+    "url": "/static/css/main.cca4f2fe.chunk.css"
+  },
+  {
+    "revision": "808623a0d9f1316fdd3b6d7823fcc550",
+    "url": "/index.html"
+  }
+];
