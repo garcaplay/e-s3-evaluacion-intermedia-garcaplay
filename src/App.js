c2454702fcbdd43370fdaea4a66047b0fcf3f5a5
Silvia García Cacho <silvia@garca.info>
2019-01-14 08:22:47 +0100

    Add array and basic jsx structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const pokeArray = [
+    {"id":1,"name":"bulbasaur","types":["poison","grass"],"evolution":null,"url":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"},
+    {"id":2,"name":"ivysaur","types":["poison","grass"],"evolution":"bulbasaur","url":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"},
+    {"id":3,"name":"venusaur","types":["poison","grass"],"evolution":"ivysaur","url":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"},
+    {"id":4,"name":"charmander","types":["fire"],"evolution":null,"url":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"},
+    {"id":5,"name":"charmeleon","types":["fire"],"evolution":"charmander","url":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"},
+    {"id":6,"name":"charizard","types":["flying","fire"],"evolution":"charmeleon","url":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"},
+    {"id":7,"name":"squirtle","types":["water"],"evolution":null,"url":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"},
+    {"id":8,"name":"wartortle","types":["water"],"evolution":"squirtle","url":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png"},
+    {"id":9,"name":"blastoise","types":["water"],"evolution":"wartortle","url":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png"},
+    {"id":10,"name":"caterpie","types":["bug"],"evolution":null,"url":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png"}
+  ];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <div className="Cards">
+          <div className="Cards_header">
+            <h1 className="Cards_header-title">PokeCartas</h1>
+          </div>
+          <ul className="Cards_list">
+            <li className="Cards_list-item">
+              <img src={pokeArray[0].url} alt="" className="Poke_img"></img>
+              <h2 className="Poke_name">{pokeArray[0].name}</h2>
+              <ul className="Poke_types-list">
+                <li className="Poke_types-item">{pokeArray[0].types[0]}</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
