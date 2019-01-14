3edc16e4664b4fc49aff29bc572fb9bf5cc76fdd
Silvia García Cacho <silvia@garca.info>
2019-01-14 09:29:57 +0100

    Correct naming of folders

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
-import TypeList from './TypeList';
+import Pokemon from './Pokemon';
 
 class PokeList extends Component {
     render() {
         const {pokeArray} = this.props;
-        const poke_li = pokeArray.map((poke, key) => {
-            return(
-                <li className="Cards_list-item" key = {key} id = {poke.id}>
-                    <div className="Poke_img-box"><img src={poke.url} alt={poke.name} className="Poke_img"></img></div>
-                    <div className="Poke_name-box">
-                    <h2 className="Poke_name">{poke.name}</h2>
-                    </div>
-                    <TypeList poke = {poke} />
-                </li>
-            );
-        })
         return (
             <ul className="Cards_list">
-                {poke_li}
+                <Pokemon pokeArray = {pokeArray}/>
             </ul>
         );
     }
---
+++ src/components/Pokemon.js
@@ -0,0 +1,27 @@
+import React, { Component, Fragment } from 'react';
+import TypeList from './TypeList';
+
+class Pokemon extends Component {
+    render() {
+        const {pokeArray} = this.props;
+        console.log(pokeArray);
+        const poke_li = pokeArray.map((poke, key) => {
+            return(
+                <li className="Cards_list-item" key = {key} id = {poke.id}>
+                    <div className="Poke_img-box"><img src={poke.url} alt={poke.name} className="Poke_img"></img></div>
+                    <div className="Poke_name-box">
+                    <h2 className="Poke_name">{poke.name}</h2>
+                    </div>
+                    <TypeList poke = {poke} />
+                </li>
+            );
+        })
+        return (
+            <Fragment>
+                {poke_li}
+            </Fragment>
+        );
+    }
+  }
+  
+  export default Pokemon;
