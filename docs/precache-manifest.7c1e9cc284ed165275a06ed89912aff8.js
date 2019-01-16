d07a299018f243ea9eb7ccbec89f56281d068595
Silvia García Cacho <silvia@garca.info>
2019-01-16 07:52:43 +0100

    Redo docs folder

---
+++ docs/precache-manifest.7c1e9cc284ed165275a06ed89912aff8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "1ea739c8110b3f3079dd",
+    "url": "./static/js/main.1ea739c8.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "./static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "1ea739c8110b3f3079dd",
+    "url": "./static/css/main.cca4f2fe.chunk.css"
+  },
+  {
+    "revision": "2b4343c132ac80c2b649d19ef80e641a",
+    "url": "./index.html"
+  }
+];
