6dd23ebcd326bf412dc656ba45c72573888a9f17
Silvia García Cacho <silvia@garca.info>
2019-01-23 17:21:32 +0100

    Update docs folder

---
+++ docs/build/precache-manifest.4561e3746acaa41d0a7702b2353cab93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "725c766e1efac91123aa",
+    "url": "./static/js/main.725c766e.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "./static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "725c766e1efac91123aa",
+    "url": "./static/css/main.cca4f2fe.chunk.css"
+  },
+  {
+    "revision": "e51eea1b7786b82f575ef2e4d75674a9",
+    "url": "./index.html"
+  }
+];
